0b1b30b4006b9a1c42a3ab8b892976a913780bcc
Eric Websmith <zhouweiguang@hotmail.com>
2021-11-22 21:50:39 +0800

    59 and 64

---
+++ LeetcodeTest/Leetcode0059.cs
@@ -0,0 +1,141 @@
+﻿namespace Leetcode0059;
+
+public class Solution
+{
+    const int EAST = 0;
+    const int SOUTH = 1;
+    const int WEST = 2;
+    const int NORTH = 3;
+
+    public int[][] GenerateMatrix(int n)
+    {
+        int[][] matrix = new int[n][];
+        for (int i = 0; i < n; i++)
+        {
+            matrix[i] = new int[n];
+        }
+
+        int x = 0;
+        int y = 0;
+
+        int direction = EAST;
+        int index = 1;
+        matrix[y][x] = 1;
+        while (index < n * n)
+        {
+            if (direction == EAST)
+            {
+
+                if (x + 1 < n && matrix[y][x + 1] == 0)
+                {
+                    x++;
+                    index++;
+                    matrix[y][x] = index;
+
+                }
+                else
+                {
+                    direction = SOUTH;
+                }
+            }
+            else if (direction == SOUTH)
+            {
+                if (y + 1 < n && matrix[y + 1][x] == 0)
+                {
+                    y++;
+                    index++;
+                    matrix[y][x] = index;
+                }
+                else
+                {
+                    direction = WEST;
+                }
+            }
+            else if (direction == WEST)
+            {
+                if (x - 1 >= 0 && matrix[y][x - 1] == 0)
+                {
+                    x--;
+                    index++;
+                    matrix[y][x] = index;
+                }
+                else
+                {
+                    direction = NORTH;
+                }
+            }
+            else if (direction == NORTH)
+            {
+                if (y - 1 >= 0 && matrix[y - 1][x] == 0)
+                {
+                    y--;
+                    index++;
+                    matrix[y][x] = index;
+                }
+                else
+                {
+                    direction = EAST;
+                }
+            }
+        }
+
+        return matrix;
+    }
+}
+
+[TestClass]
+public class SolutionTest
+{
+    [TestMethod]
+    public void Test3()
+    {
+        Solution solution = new Solution();
+        int[][] expected = new int[][]
+        {
+            new int[]{ 1,2,3 },
+            new int[]{ 8,9,4 },
+            new int[]{ 7,6,5 }
+        };
+
+        int[][] actual = solution.GenerateMatrix(3);
+        foreach (int[] line in actual)
+        {
+            Console.WriteLine($"-------------{3}-----------");
+            string sline = string.Join(",", line.Select(x => x.ToString()));
+            Console.WriteLine(line);
+        }
+
+        Assert.AreEqual(expected.Length, actual.Length);
+        for (int i = 0; i < expected.Length; i++)
+        {
+            for (int j = 0; j < expected[i].Length; j++)
+            {
+                Assert.AreEqual(expected[i][j], actual[i][j]);
+            }
+        }
+    }
+
+    [TestMethod]
+    public void Test()
+    {
+        Solution solution = new Solution();
+
+        for (int i = 1; i <= 20; i++)
+        {
+            Console.WriteLine($"-------------{i}-----------");
+            int[][] actual = solution.GenerateMatrix(i);
+            foreach (int[] line in actual)
+            {
+
+                string sline = string.Empty;
+                for (int j = 0; j < line.Length; j++)
+                {
+                    sline += line[j].ToString("000") + ",";
+                }
+
+                Console.WriteLine(sline);
+            }
+        }
+
+    }
+}
---
+++ LeetcodeTest/Leetcode0064.cs
@@ -0,0 +1,47 @@
+﻿namespace Leetcode0064;
+
+public class Solution
+{
+    public int MinPathSum(int[][] grid)
+    {
+        int m = grid.Length;
+        int n = grid[0].Length;
+
+        for (int i = 1; i < m; i++)
+        {
+            grid[i][0] += grid[i - 1][0];
+        }
+
+        for (int j = 1; j < n; j++)
+        {
+            grid[0][j] += grid[0][j - 1];
+        }
+
+        for (int i = 1; i < m; i++)
+        {
+            for (int j = 1; j < n; j++)
+            {
+                grid[i][j] += Math.Min(grid[i - 1][j], grid[i][j - 1]);
+            }
+        }
+
+        return grid[m - 1][n - 1];
+
+    }
+}
+
+[TestClass]
+public class SolutionTest
+{
+    [DataRow(1, "[[1,3,1],[1,5,1],[4,2,1]]", 7)]
+    [DataRow(2, "[[1,2,3],[4,5,6]]", 12)]
+    [DataRow(3, "[[1,200,3],[4,200,6]]", 210)]
+    [DataRow(4, "[[9,1,4,8]]", 22)]
+    [DataTestMethod]
+    public void Test(int order, string matrix, int expected)
+    {
+        int[][] grid = Helper.Convert2D(matrix);
+        int actual = (new Solution()).MinPathSum(grid);
+        Assert.AreEqual(expected, actual);
+    }
+}
