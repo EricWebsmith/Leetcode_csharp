072a319cb9415b516e513736365ac8c5c62cc6fb
Eric Websmith <zhouweiguang@hotmail.com>
2021-11-26 12:12:53 +0800

    3 11/26

---
+++ LeetcodeTest/Leetcode0011.cs
@@ -0,0 +1,45 @@
+﻿namespace Leetcode0011;
+
+public class Solution
+{
+    public int MaxArea(int[] height)
+    {
+        int left = 0;
+        int right = height.Length - 1;
+
+        int maxArea = 0;
+        while (left < right)
+        {
+            int area = (right - left) * Math.Min(height[left], height[right]);
+            maxArea = Math.Max(maxArea, area);
+            if (height[left] > height[right])
+            {
+                right--;
+            }
+            else
+            {
+                left++;
+            }
+        }
+        return maxArea;
+    }
+}
+
+[TestClass]
+public class SolutionTest
+{
+    [DataRow(1, "1,8,6,2,5,4,8,3,7", 49)]
+    [DataRow(2, "1,1", 1)]
+    [DataRow(3, "4,3,2,1,4", 16)]
+    [DataRow(4, "1,2,1", 2)]
+    [DataRow(5, "0, 0, 0", 0)]
+    [DataRow(6, "10000, 10000, 0", 10000)]
+    [DataRow(7, "1000, 10000, 0", 1000)]
+    [DataTestMethod]
+    public void Test(int order, string heightStr, int expected)
+    {
+        int[] heigt = Helper.Convert1D(heightStr);
+        int actual = (new Solution()).MaxArea(heigt);
+        Assert.AreEqual(expected, actual);
+    }
+}
---
+++ LeetcodeTest/Leetcode0322.cs
@@ -0,0 +1,51 @@
+﻿namespace Leetcode0322;
+
+public class Solution
+{
+    public int CoinChange(int[] coins, int amount)
+    {
+        Array.Sort(coins);
+        int[] dp = new int[amount + 1];
+        Array.Fill(dp, amount + 1);
+        dp[0] = 0;
+
+        for (int i = 1; i <= amount; i++)
+        {
+            foreach (int coin in coins)
+            {
+                if (i - coin >= 0)
+                {
+                    dp[i] = Math.Min(dp[i], dp[i - coin] + 1);
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+
+        if(dp[amount] == amount + 1)
+        {
+            return -1;
+        }
+
+        return dp[amount];
+    }
+}
+
+[TestClass]
+public class SolutionTest
+{
+    [DataRow(1, "1,2,5", 11, 3)]
+    [DataRow(2, "2", 3, -1)]
+    [DataRow(3, "1", 0, 0)]
+    [DataRow(4, "1", 1, 1)]
+    [DataRow(5, "1", 2, 2)]
+    [DataTestMethod]
+    public void Test(int order, string coinsStr,int amount, int expected)
+    {
+        int[] coins = Helper.Convert1D(coinsStr);
+        int actual = (new Solution()).CoinChange(coins, amount);
+        Assert.AreEqual(expected, actual);
+    }
+}
