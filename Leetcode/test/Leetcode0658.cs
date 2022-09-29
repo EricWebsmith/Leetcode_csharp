e2428ab68dee37dfbd43d003bdcb86322039ae55
eric <eric@3PO>
2022-09-29 20:04:10 +0800

    2422

---
+++ Leetcode/test/Leetcode0658.cs
@@ -0,0 +1,62 @@
+﻿namespace Leetcode0658;
+
+
+public class Solution
+{
+    public IList<int> FindClosestElements(int[] arr, int k, int x)
+    {
+        int n = arr.Length;
+        int left = 0;
+        int right = n;
+        while(left < right)
+        {
+            int mid = (left + right) >> 1;
+            if (arr[mid] < x)
+            {
+                left = mid + 1;
+            } 
+            else
+            {
+                right = mid;
+            }
+        }
+        left--;
+
+        int l = 0;
+        while (l < k)
+        {
+            if (right==n || (left>=0 && x - arr[left] <= arr[right] - x))
+            {
+                left--;
+            } 
+            else
+            {
+                right++;
+            }
+            l++;
+        }
+
+        return arr.Skip(left+1).Take(k).ToList();
+    }
+}
+
+
+[TestClass]
+public class SolutionTests
+{
+    private void TestBase(string arrStr, int k, int x, string expectedStr)
+    {
+        int[] arr = arrStr.LeetcodeToArray();
+        IList<int> actual = new Solution().FindClosestElements(arr, k, x);
+        Assert.AreEqual(actual.ToLeetcode(), expectedStr);
+    }
+
+    [TestMethod] public void Test1() { TestBase("[1,2,3,4,5]", 4, 3, "[1,2,3,4]"); }
+    [TestMethod] public void Test2() { TestBase("[1,2,3,4,5]", 4, -1, "[1,2,3,4]"); }
+    [TestMethod] public void Test3() { TestBase("[1, 1, 1, 10, 10, 10]", 1, 9, "[10]"); }
+}
+
+/*
+Runtime: 196 ms, faster than 94.26% of C# online submissions for Find K Closest Elements.
+Memory Usage: 54.1 MB, less than 11.48% of C# online submissions for Find K Closest Elements.
+ */
---
+++ Leetcode/test/Leetcode2422.cs
@@ -0,0 +1,61 @@
+﻿namespace Leetcode2422;
+
+public class Solution
+{
+    public int MinimumOperations(int[] nums)
+    {
+        int n = nums.Length;
+        int left = 0;
+        int right = n - 1;
+        int leftSum = nums[left];
+        int rightSum = nums[right];
+
+        int operations = 0;
+        while (left < right)
+        {
+            if (leftSum<rightSum)
+            {
+                left++;
+                leftSum += nums[left];
+                operations++;
+            } else if (rightSum < leftSum)
+            {
+                right--;
+                rightSum += nums[right];
+                operations++;
+            }
+            else
+            {
+                left++;
+                leftSum += nums[left];
+                right--;
+                rightSum += nums[right];
+            }
+        }
+
+        return operations;
+    }
+}
+
+
+[TestClass]
+public class SolutionTests
+{
+    private void TestBase(string numsStr, int expected)
+    {
+        int[] nums = numsStr.LeetcodeToArray();
+        int actual = new Solution().MinimumOperations(nums);
+        Assert.AreEqual(expected, actual);
+    }
+
+    [TestMethod] public void Test1() { TestBase("[4,3,2,1,2,3,1]", 2); }
+    [TestMethod] public void Test2() { TestBase("[1,2,3,4]", 3); }
+    //[TestMethod] public void Test3() { TestBase(); }
+    //[TestMethod] public void Test4() { TestBase(); }
+
+}
+
+/*
+Runtime: 400 ms, faster than 100.00% of C# online submissions for Merge Operations to Turn Array Into a Palindrome.
+Memory Usage: 49.7 MB, less than 100.00% of C# online submissions for Merge Operations to Turn Array Into a Palindrome.
+ */
